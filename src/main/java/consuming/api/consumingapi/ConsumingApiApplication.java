eedd3200984e712d12d761035faa4dcbeb7d3f1c
LuisPereir4 <49247322+LuisPereir4@users.noreply.github.com>
2019-11-16 10:44:26 -0300

    Initial commit

---
+++ src/main/java/consuming/api/consumingapi/ConsumingApiApplication.java
@@ -0,0 +1,13 @@
+package consuming.api.consumingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/consuming/api/consumingapi/ConsumingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package consuming.api.consumingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
